a3a768ee85e225c9ab3a2f9db8052250948a3ffc
i-cortez <56184561+i-cortez@users.noreply.github.com>
2020-01-05 00:32:43 -0800

    Update assignment1.asm

--- Assignment1/assignment1.asm
+++ Assignment1/assignment1.asm
@@ -22,11 +22,11 @@ main:		# Start of code section
 	sll	$t1, $t0, 2
 	or 	$t2, $t1, $t0
 	sll	$t0, $t2, 4
-	or		$t1, $t0, $t2
+	or	$t1, $t0, $t2
 	sll	$t0, $t1,8
-	or		$t2, $t0, $t1
+	or	$t2, $t0, $t1
 	sll	$t1, $t2, 16
-	or		$t0, $t1, $t2
+	or	$t0, $t1, $t2
 	nor	$t1, $t0, $0
 	
 	
@@ -39,7 +39,7 @@ main:		# Start of code section
 
 	ori	$t0, $0, 0x01
 	sll	$t1, $t0, 3
-	or		$t2, $t1, $t0
+	or	$t2, $t1, $t0
 	nor	$t0, $t2, $0
 	sll	$t1, $t0, 4
 	nor	$t0, $t1, $t2
@@ -47,7 +47,7 @@ main:		# Start of code section
 	nor	$t2, $t1, $t0
 	sll	$t0, $t2, 16
 	srl	$t2, $t0, 16
-	or		$t1, $t2, $t0
+	or	$t1, $t2, $t0
 	
 	#
 	# Start with the given pattern and use only
@@ -58,7 +58,7 @@ main:		# Start of code section
 	
 	ori	$t0, $0, 0x01
 	nor	$t2, $t0, $t0
-	or		$t1, $t2, $t0
+	or	$t1, $t2, $t0
 	
 	#
 	# Exit the program.
